bf6eecc587f5fbb2363e7a657c4a6a61e8ef663e
Артём Бочкарёв <chipalinchik@gmail.com>
2024-03-01 23:28:14 +0500

    fix

--- __tests__/gendif.test.js
+++ __tests__/gendif.test.js
@@ -3,7 +3,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { readFileSync } from 'fs';
 
-import parseFilesAndGenDiff from '../src/main.js';
+import parseFilesAndGenDiff from '../src/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
